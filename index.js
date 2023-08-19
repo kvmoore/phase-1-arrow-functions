58c1858f042f11327311a54bbd15630a63d4a2c3
Kenneth <kvmoore097@gmail.com>
2023-08-19 11:42:01 -0500

    phase-1 arrow functions

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+const divide = (a, b) => a / b;
+divide(2000,100);    
+
+
+const square = (num) => {
+    return num * 2;
+}
+square(2);
+
+
+const add = (a,b) => a + b;
+add(3,4);
+
+/*
+=> {  add   }
+
+(add(a,b))
+
+a + b
+
+3 + 4
+
+
+
+}*/   
